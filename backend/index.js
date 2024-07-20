bf18e7eb921351040ad25b1e8d37a50e662b79b6
rahulsaini-js <itsrahulsaini21@gmail.com>
2024-07-20 18:24:30 +0530

    user register functionality in backend

--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,12 @@ const dotenv = require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
-const jwt=require("jsonwebtoken")
-const {authenticateToken} =require("./utilities")
+const jwt = require("jsonwebtoken");
+const { authenticateToken } = require("./utilities");
+const User = require("./models/user.model");
 
 mongoose.connect(process.env.connection_string);
 const app = express();
-
 
 app.use(express.json());
 app.use(
@@ -19,8 +19,46 @@ app.get("/", (req, res) => {
   res.json({ data: "hello" });
 });
 
+// Create Account
+app.post("/create-account", async (req, res) => {
+  const { fullName, email, password } = req.body;
+  if (!fullName) {
+    return res
+      .status(400)
+      .json({ error: true, message: "Full Name is required" });
+  }
+  if (!email) {
+    return res.status(400).json({ error: true, message: "Email is required" });
+  }
+  if (!password) {
+    return res
+      .status(400)
+      .json({ error: true, message: "Password is required" });
+  }
 
-
+  const isUser = await User.findOne({ email: email });
+  if (isUser) {
+    return res.json({
+      error: true,
+      message: "User already exist",
+    });
+  }
+  const user = new User({
+    fullName,
+    email,
+    password,
+  });
+  await user.save();
+  const accessToken = jwt.sign({ user }, process.env.ACCESS_TOKEN_SECRET, {
+    expiresIn: "36000m",
+  });
+  return res.json({
+    error: false,
+    user,
+    accessToken,
+    message: "Registration Successful",
+    });
+});
 
 app.listen(8000);
 module.exports = app;
