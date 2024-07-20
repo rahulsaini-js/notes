e5aeff606525ce9e4ccaa2f64c1527b3dc6374d3
rahulsaini-js <itsrahulsaini21@gmail.com>
2024-07-20 16:55:21 +0530

    user model connect to database

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,14 @@
+const dotenv = require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
+const jwt=require("jsonwebtoken")
+const {authenticateToken} =require("./utilities")
+
+mongoose.connect(process.env.connection_string);
 const app = express();
+
+
 app.use(express.json());
 app.use(
   cors({
@@ -10,5 +18,9 @@ app.use(
 app.get("/", (req, res) => {
   res.json({ data: "hello" });
 });
+
+
+
+
 app.listen(8000);
 module.exports = app;
