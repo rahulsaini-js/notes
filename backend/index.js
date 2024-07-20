603f6f4c3484fc84a2637457b51ededabbecf34a
rahulsaini-js <itsrahulsaini21@gmail.com>
2024-07-20 18:34:04 +0530

    login into account backend route

--- backend/index.js
+++ backend/index.js
@@ -57,8 +57,48 @@ app.post("/create-account", async (req, res) => {
     user,
     accessToken,
     message: "Registration Successful",
-    });
+  });
 });
+
+//Login into account
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email) {
+    return res.status(400).json({ message: "Email is required" });
+  }
+
+  if (!password) {
+    return res.status(400).json({ message: "Password is required" });
+  }
+
+  const userInfo = await User.findOne({ email: email });
+
+  if (!userInfo) {
+    return res.status(400).json({ message: "User not found" });
+  }
+
+  if (userInfo.email == email && userInfo.password == password) {
+    const user = { user: userInfo };
+    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+      expiresIn: "36000m",
+    });
+    return res.json({
+      error: false,
+      message: "Login Successful",
+      email,
+      accessToken,
+    });
+  } else {
+    return res.status(400).json({
+      error: true,
+      message: "Invalid Credentials",
+    });
+  }
+});
+
+//Add note
+
 
 app.listen(8000);
 module.exports = app;
