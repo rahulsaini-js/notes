7904766c0fed4f68c09c178f14163317402f29ca
rahulsaini-js <itsrahulsaini21@gmail.com>
2024-07-21 12:38:57 +0530

    login api integration

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -3,6 +3,7 @@ import Navbar from "../../components/Navbar/Navbar";
 import { Link, useNavigate } from "react-router-dom";
 import PasswordInput from "../../components/Input/PasswordInput";
 import { validateEmail } from "../../utils/helper";
+import axiosInstance from "../../utils/axiosInstance"
 
 const Login = () => {
   const [email, setEmail] = useState("");
