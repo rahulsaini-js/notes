5320b9550133b362edb3e43673e3b26e4ed88919
rahulsaini-js <itsrahulsaini21@gmail.com>
2024-07-21 22:53:58 +0530

    add pre installation

--- backend/index.js
+++ backend/index.js
@@ -4,12 +4,15 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const jwt = require("jsonwebtoken");
 const { authenticateToken } = require("./utilities");
+const path =require('path')
 
 const User = require("./models/user.model");
 const Note = require("./models/note.model");
 
 mongoose.connect(process.env.connection_string);
 const app = express();
+
+const __dirname=path.resolve();
 
 app.use(express.json());
 app.use(
@@ -308,6 +311,11 @@ app.get("/search-notes", authenticateToken, async (req, res) => {
     });
   }
 });
+
+app.use(express.static(path.join(__dirname,'/frontend/dist')))
+app.get('*',(req,res)=>{
+  res.sendFile(path.join(__dirname,'frontend','dist','index.html'))
+})
 
 app.listen(8000);
 module.exports = app;
